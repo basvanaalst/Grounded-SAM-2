6d3135d9dd4dce1baee6181894d85a0689399550
Bvanaalst <b.vanaalst123@gmail.com>
2025-02-24 13:57:57 +0100

    use plant images for grounded-sam-2

---
+++ grounded_sam.py
@@ -0,0 +1,130 @@
+# Download pre-trained weights for GroundingDINO after cd Grounded-Segment-Anything
+# wget https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
+
+# Download weights for SAM
+#! curl https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -o sam_vit_h_4b8939.pth
+
+# Install GroundingDINO, SAM and all other dependencies
+#! python -m pip install -e segment_anything
+#! python -m pip install -e GroundingDINO
+
+# Optional for diffusion model and LLM stuff, might need one of these for the installation to work
+#! pip install diffusers transformers accelerate scipy safetensors
+
+import os
+import numpy as np
+import torch
+import cv2
+
+import torch
+import supervision as sv
+import torchvision
+
+from GroundingDINO.groundingdino.util import box_ops
+from GroundingDINO.groundingdino.util.inference import Model
+from segment_anything.segment_anything import sam_model_registry, SamPredictor
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+GROUNDING_DINO_CONFIG_PATH = "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+GROUNDING_DINO_CHECKPOINT_PATH = "./groundingdino_swint_ogc.pth"
+
+SAM_CHECKPOINT_PATH = "./sam_vit_h_4b8939.pth"
+
+input_dir = "images"
+output_dir = "images_results"
+
+# Building GroundingDINO
+groundingdino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH, device=DEVICE)
+
+print("dino done")
+
+# Building SAM
+sam = sam_model_registry["vit_h"](checkpoint=SAM_CHECKPOINT_PATH)
+sam.to(DEVICE)
+sam_predictor = SamPredictor(sam)
+
+print("sam done")
+
+#semantic segmentation for all files in images folder
+for file in os.listdir(input_dir):
+    if file.lower().endswith(".jpg"):
+        CLASSES = ["plants"]
+        BOX_THRESHOLD = 0.25
+        TEXT_THRESHOLD = 0.25
+        NMS_THRESHOLD = 0.8
+
+        input_path = os.path.join(input_dir, file)
+        image = cv2.imread(input_path)
+
+        detections = groundingdino_model.predict_with_classes(
+            image=image,
+            classes=CLASSES,
+            box_threshold=BOX_THRESHOLD,
+            text_threshold=TEXT_THRESHOLD
+        )
+
+        # NMS post process
+        print(f"Before NMS: {len(detections.xyxy)} boxes")
+        nms_idx = torchvision.ops.nms(
+            torch.from_numpy(detections.xyxy), 
+            torch.from_numpy(detections.confidence), 
+            NMS_THRESHOLD
+        ).numpy().tolist()
+
+        detections.xyxy = detections.xyxy[nms_idx]
+        detections.confidence = detections.confidence[nms_idx]
+        detections.class_id = detections.class_id[nms_idx]
+
+        print(f"After NMS: {len(detections.xyxy)} boxes")
+
+        def segment(sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
+            sam_predictor.set_image(image)
+            result_masks = []
+            for box in xyxy:
+                masks, scores, _ = sam_predictor.predict(
+                    box=box,
+                    multimask_output=True
+                )
+                index = np.argmax(scores)
+                result_masks.append(masks[index])
+            return np.array(result_masks)
+
+        detections.mask = segment(
+            sam_predictor=sam_predictor,
+            image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
+            xyxy=detections.xyxy
+        )
+
+        # change color of masks!
+
+        box_annotator = sv.BoundingBoxAnnotator()
+        label_annotator = sv.LabelAnnotator()
+        mask_annotator = sv.MaskAnnotator(color=sv.ColorPalette.DEFAULT)
+        labels = [
+            f"{CLASSES[class_id]} {confidence:0.2f}" 
+            for _, _, confidence, class_id, _, _ 
+            in detections]
+        annotated_image = box_annotator.annotate(scene=image.copy(), detections=detections)
+        annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+        annotated_image = mask_annotator.annotate(scene=annotated_image, detections=detections)
+       
+        mask_arrays = [detections.mask[i] for i in range(len(detections.mask))]
+        combined_mask = np.stack(mask_arrays, axis=0)
+        final_mask = np.max(combined_mask, axis=0) 
+        mask_image = (final_mask * 255).astype(np.uint8)
+
+        image_source = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA) if image.shape[2] == 3 else image.copy()
+        annotated_frame = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2BGRA) if annotated_image.shape[2] == 3 else annotated_image.copy()
+
+        result_image = image_source.copy()
+        result_image[:, :, 3] = mask_image
+
+        result_images = [(mask_image, "mask"), (annotated_frame, "annotated_frame"), (result_image, "result")]
+        for image, category in result_images:
+            file_without_ext = os.path.splitext(file)[0]
+            filename = f"{category}_{file_without_ext}.png"
+            output_path = os.path.join(output_dir, filename)
+            cv2.imwrite(output_path, image)
+            print(f"Saved {output_path}")
+
--- grounded_sam2_hf_model_demo.py
+++ grounded_sam2_hf_model_demo.py
@@ -19,18 +19,19 @@ Hyper parameters
 """
 parser = argparse.ArgumentParser()
 parser.add_argument('--grounding-model', default="IDEA-Research/grounding-dino-tiny")
-parser.add_argument("--text-prompt", default="car. tire.")
-parser.add_argument("--img-path", default="notebooks/images/truck.jpg")
+parser.add_argument("--text-prompt", default="plants.")
+parser.add_argument("--input-dir", default="images")
 parser.add_argument("--sam2-checkpoint", default="./checkpoints/sam2.1_hiera_large.pt")
 parser.add_argument("--sam2-model-config", default="configs/sam2.1/sam2.1_hiera_l.yaml")
-parser.add_argument("--output-dir", default="outputs/test_sam2.1")
+parser.add_argument("--output-dir", default="outputs")
 parser.add_argument("--no-dump-json", action="store_true")
 parser.add_argument("--force-cpu", action="store_true")
 args = parser.parse_args()
 
 GROUNDING_MODEL = args.grounding_model
 TEXT_PROMPT = args.text_prompt
-IMG_PATH = args.img_path
+
+INPUT_DIR = args.input_dir
 SAM2_CHECKPOINT = args.sam2_checkpoint
 SAM2_MODEL_CONFIG = args.sam2_model_config
 DEVICE = "cuda" if torch.cuda.is_available() and not args.force_cpu else "cpu"
@@ -44,11 +45,6 @@ OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 # use bfloat16
 torch.autocast(device_type=DEVICE, dtype=torch.bfloat16).__enter__()
 
-if torch.cuda.get_device_properties(0).major >= 8:
-    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
-
 # build SAM2 image predictor
 sam2_checkpoint = SAM2_CHECKPOINT
 model_cfg = SAM2_MODEL_CONFIG
@@ -60,95 +56,98 @@ model_id = GROUNDING_MODEL
 processor = AutoProcessor.from_pretrained(model_id)
 grounding_model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(DEVICE)
 
+for file in os.listdir(INPUT_DIR):
+    if file.lower().endswith(".jpg"):
 
-# setup the input image and text prompt for SAM 2 and Grounding DINO
-# VERY important: text queries need to be lowercased + end with a dot
-text = TEXT_PROMPT
-img_path = IMG_PATH
+        # setup the input image and text prompt for SAM 2 and Grounding DINO
+        # VERY important: text queries need to be lowercased + end with a dot
+        text = TEXT_PROMPT
+        input_path = os.path.join(INPUT_DIR, file)
+        image = Image.open(input_path)
 
-image = Image.open(img_path)
+        sam2_predictor.set_image(np.array(image.convert("RGB")))
 
-sam2_predictor.set_image(np.array(image.convert("RGB")))
+        inputs = processor(images=image, text=text, return_tensors="pt").to(DEVICE)
+        with torch.no_grad():
+            outputs = grounding_model(**inputs)
 
-inputs = processor(images=image, text=text, return_tensors="pt").to(DEVICE)
-with torch.no_grad():
-    outputs = grounding_model(**inputs)
+        results = processor.post_process_grounded_object_detection(
+            outputs,
+            inputs.input_ids,
+            box_threshold=0.4,
+            text_threshold=0.3,
+            target_sizes=[image.size[::-1]]
+        )
 
-results = processor.post_process_grounded_object_detection(
-    outputs,
-    inputs.input_ids,
-    box_threshold=0.4,
-    text_threshold=0.3,
-    target_sizes=[image.size[::-1]]
-)
+        """
+        Results is a list of dict with the following structure:
+        [
+            {
+                'scores': tensor([0.7969, 0.6469, 0.6002, 0.4220], device='cuda:0'), 
+                'labels': ['car', 'tire', 'tire', 'tire'], 
+                'boxes': tensor([[  89.3244,  278.6940, 1710.3505,  851.5143],
+                                [1392.4701,  554.4064, 1628.6133,  777.5872],
+                                [ 436.1182,  621.8940,  676.5255,  851.6897],
+                                [1236.0990,  688.3547, 1400.2427,  753.1256]], device='cuda:0')
+            }
+        ]
+        """
 
-"""
-Results is a list of dict with the following structure:
-[
-    {
-        'scores': tensor([0.7969, 0.6469, 0.6002, 0.4220], device='cuda:0'), 
-        'labels': ['car', 'tire', 'tire', 'tire'], 
-        'boxes': tensor([[  89.3244,  278.6940, 1710.3505,  851.5143],
-                        [1392.4701,  554.4064, 1628.6133,  777.5872],
-                        [ 436.1182,  621.8940,  676.5255,  851.6897],
-                        [1236.0990,  688.3547, 1400.2427,  753.1256]], device='cuda:0')
-    }
-]
-"""
+        # get the box prompt for SAM 2
+        input_boxes = results[0]["boxes"].cpu().numpy()
 
-# get the box prompt for SAM 2
-input_boxes = results[0]["boxes"].cpu().numpy()
-
-masks, scores, logits = sam2_predictor.predict(
-    point_coords=None,
-    point_labels=None,
-    box=input_boxes,
-    multimask_output=False,
-)
-
-
-"""
-Post-process the output of the model to get the masks, scores, and logits for visualization
-"""
-# convert the shape to (n, H, W)
-if masks.ndim == 4:
-    masks = masks.squeeze(1)
+        masks, scores, logits = sam2_predictor.predict(
+            point_coords=None,
+            point_labels=None,
+            box=input_boxes,
+            multimask_output=False,
+        )
 
 
-confidences = results[0]["scores"].cpu().numpy().tolist()
-class_names = results[0]["labels"]
-class_ids = np.array(list(range(len(class_names))))
+        """
+        Post-process the output of the model to get the masks, scores, and logits for visualization
+        """
+        # convert the shape to (n, H, W)
+        if masks.ndim == 4:
+            masks = masks.squeeze(1)
 
-labels = [
-    f"{class_name} {confidence:.2f}"
-    for class_name, confidence
-    in zip(class_names, confidences)
-]
 
-"""
-Visualize image with supervision useful API
-"""
-img = cv2.imread(img_path)
-detections = sv.Detections(
-    xyxy=input_boxes,  # (n, 4)
-    mask=masks.astype(bool),  # (n, h, w)
-    class_id=class_ids
-)
+        confidences = results[0]["scores"].cpu().numpy().tolist()
+        class_names = results[0]["labels"]
+        class_ids = np.array(list(range(len(class_names))))
 
-"""
-Note that if you want to use default color map,
-you can set color=ColorPalette.DEFAULT
-"""
-box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = box_annotator.annotate(scene=img.copy(), detections=detections)
+        labels = [
+            f"{class_name} {confidence:.2f}"
+            for class_name, confidence
+            in zip(class_names, confidences)
+        ]
 
-label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
-cv2.imwrite(os.path.join(OUTPUT_DIR, "groundingdino_annotated_image.jpg"), annotated_frame)
+        """
+        Visualize image with supervision useful API
+        """
+        img = cv2.imread(input_path)
+        detections = sv.Detections(
+            xyxy=input_boxes,  # (n, 4)
+            mask=masks.astype(bool),  # (n, h, w)
+            class_id=class_ids
+        )
 
-mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
-annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
-cv2.imwrite(os.path.join(OUTPUT_DIR, "grounded_sam2_annotated_image_with_mask.jpg"), annotated_frame)
+        """
+        Note that if you want to use default color map,
+        you can set color=ColorPalette.DEFAULT
+        """
+        filename = os.path.splitext(file)[0]
+        
+        box_annotator = sv.BoxAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+        annotated_frame = box_annotator.annotate(scene=img.copy(), detections=detections)
+
+        label_annotator = sv.LabelAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+        annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
+        cv2.imwrite(os.path.join(OUTPUT_DIR, f"{filename}_groundingdino_annotated_image.jpg"), annotated_frame)
+
+        mask_annotator = sv.MaskAnnotator(color=ColorPalette.from_hex(CUSTOM_COLOR_MAP))
+        annotated_frame = mask_annotator.annotate(scene=annotated_frame, detections=detections)
+        cv2.imwrite(os.path.join(OUTPUT_DIR, f"{filename}_grounded_sam2_annotated_image_with_mask.jpg"), annotated_frame)
 
 
 """
@@ -168,7 +167,7 @@ if DUMP_JSON_RESULTS:
     scores = scores.tolist()
     # save the results in standard format
     results = {
-        "image_path": img_path,
+        "image_path": INPUT_DIR,
         "annotations" : [
             {
                 "class_name": class_name,
